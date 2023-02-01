10eb0a6da085b6af1a18fa45a30a7cb8c8ab0a24
arjun11120 <arjunarjun2159@gmail.com>
2023-02-01 17:38:21 +0530

    new feature freature2 branch added

--- src/components/header/src/index.js
+++ src/components/header/src/index.js
@@ -5,7 +5,6 @@ import reportWebVitals from './reportWebVitals';
 import Store from './redux/store';
 import { Provider } from 'react-redux';
 
-console.log("its working")
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={Store}>
